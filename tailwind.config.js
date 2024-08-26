9342ae6f52c353dee1ccbb093bf3a5da27b0a4b9
Deniz Petzold <deniz.petzold@gmail.com>
2024-08-26 21:37:36 +0200

    fixed flowbite, added react icons

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,17 @@
+const flowbite = require("flowbite-react/tailwind");
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    './src/**/*.{js,jsx,ts,tsx}',
-    'node_modules/flowbite-react/lib/esm/**/*.js'
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    flowbite.content(),
   ],
   theme: {
     extend: {},
   },
   plugins: [
-    require('flowbite/plugin')
+    flowbite.plugin()
   ],
 }
 
