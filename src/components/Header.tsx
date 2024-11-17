42d3a46d64a6f8d0af06df02e622d2a4873a6277
Deniz Petzold <deniz.petzold@gmail.com>
2024-11-17 23:55:26 +0100

    fixed spacing on header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -22,7 +22,7 @@ export default function Header() {
                     <h1 className='text-4xl text-yellow-300 leading-none'>Rootrack</h1>
                 </Link>
 
-                <div className='w-full flex items-center justify-center md:gap-8 xl:gap-24 2xl:gap-48 max-w-[800px] collapse md:visible'>
+                <div className='w-full flex items-center justify-center md:gap-8 xl:gap-12 2xl:gap-16 max-w-[800px] collapse md:visible'>
                     <Link to='/faq' className={`text-2xl ${pathname == "/faq" ? 'text-gray-500 pointer-events-none' : 'text-yellow-400 hover:text-yellow-300'}`}>FAQ</Link>
                     <Link to='https://github.com/computerd36/rootrack' className='text-2xl text-yellow-400 hover:text-yellow-300' target='_blank'>Repository</Link>
                     <Link to='/about' className={`text-2xl ${pathname == "/about" ? 'text-gray-500 pointer-events-none' : 'text-yellow-400 hover:text-yellow-300'}`}>About</Link>
